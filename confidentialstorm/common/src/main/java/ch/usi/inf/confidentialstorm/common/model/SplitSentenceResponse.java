01e8ab3131b01815601ee56f74ed41c6be2b9e96
Luca Di Bello <lucadibello@proton.me>
2025-11-08 17:25:36 +0000

    fix: SplitSentenceResponse is not serialized/deserialized correctly

--- confidentialstorm/common/src/main/java/ch/usi/inf/confidentialstorm/common/model/SplitSentenceResponse.java
+++ confidentialstorm/common/src/main/java/ch/usi/inf/confidentialstorm/common/model/SplitSentenceResponse.java
@@ -3,6 +3,7 @@ package ch.usi.inf.confidentialstorm.common.model;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public record SplitSentenceResponse(List<String> words) implements Serializable {
@@ -13,6 +14,8 @@ public record SplitSentenceResponse(List<String> words) implements Serializable 
         if (words == null) {
             throw new IllegalArgumentException("Words cannot be null");
         }
-        words = List.copyOf(new ArrayList<>(words));
+        // NOTE: List.copyOf would return a list whose implementation is not guaranteed to be serializable.
+        // Therefore, we return an unmodifiable view over a new array list.
+        words = Collections.unmodifiableList(new ArrayList<>(words));
     }
 }
