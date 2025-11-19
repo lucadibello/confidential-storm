638ac59b7b8327c246f40d9647d7f5a430adb472
Luca Di Bello <lucadibello@proton.me>
2025-11-19 14:51:31 +0100

    fix: now using EncryptedValue objects rather than JokeReader.Joke

--- confidentialstorm/host/src/main/java/ch/usi/inf/confidentialstorm/host/spouts/RandomJokeSpout.java
+++ confidentialstorm/host/src/main/java/ch/usi/inf/confidentialstorm/host/spouts/RandomJokeSpout.java
@@ -9,6 +9,7 @@ import org.apache.storm.tuple.Values;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.usi.inf.confidentialstorm.common.crypto.model.EncryptedValue;
 import ch.usi.inf.confidentialstorm.host.util.JokeReader;
 
 import java.io.IOException;
@@ -19,7 +20,7 @@ import java.util.Random;
 public class RandomJokeSpout implements IRichSpout {
   private static final long EMIT_DELAY_MS = 250;
   private SpoutOutputCollector collector;
-  private List<JokeReader.Joke> jokes;
+  private List<EncryptedValue> encryptedJokes;
   private Random rand;
   private int taskId;
   private final Logger LOG = LoggerFactory.getLogger(getClass());
@@ -34,9 +35,9 @@ public class RandomJokeSpout implements IRichSpout {
     // we need to load the jokes in memory
     JokeReader jokeReader = new JokeReader();
     try {
-        jokes = jokeReader.readAll("jokes.enc.json");
+      encryptedJokes = jokeReader.readAll("jokes.enc.json");
     } catch (IOException e) {
-        throw new RuntimeException(e);
+      throw new RuntimeException(e);
     }
     // save the collector for emitting tuples <Joke, String>
     this.collector = collector;
@@ -60,21 +61,21 @@ public class RandomJokeSpout implements IRichSpout {
   @Override
   public void nextTuple() {
     // generate the next random joke
-    int idx = rand.nextInt(jokes.size());
-    JokeReader.Joke curr = jokes.get(idx);
-    LOG.info("[RandomJokeSpout {}] Emitting joke {}", taskId, curr);
-    collector.emit(new Values(curr.id, curr.category, curr.rating, curr.body));
+    int idx = rand.nextInt(encryptedJokes.size());
+    EncryptedValue currentJoke = encryptedJokes.get(idx);
+    LOG.info("[RandomJokeSpout {}] Emitting joke {}", taskId, currentJoke);
+    collector.emit(new Values(currentJoke));
     // sleep for a while to avoid starving the topology
     try {
-        Thread.sleep(EMIT_DELAY_MS);
+      Thread.sleep(EMIT_DELAY_MS);
     } catch (InterruptedException e) {
-        // do nothing
+      // do nothing
     }
   }
 
   @Override
   public void declareOutputFields(OutputFieldsDeclarer declarer) {
-    declarer.declare(new Fields("id", "category", "rating", "body"));
+    declarer.declare(new Fields("body"));
   }
 
   @Override
