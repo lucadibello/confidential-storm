d6d145fe3d969ccccaff51b241543945211f2f00
Luca Di Bello <lucadibello@proton.me>
2025-11-23 00:01:14 +0000

    feat: EnclaveLogger now supports log levels + using record class for logger impl

--- confidentialstorm/enclave/src/main/java/ch/usi/inf/confidentialstorm/enclave/EnclaveConfig.java
+++ confidentialstorm/enclave/src/main/java/ch/usi/inf/confidentialstorm/enclave/EnclaveConfig.java
@@ -15,4 +15,16 @@ public final class EnclaveConfig {
      * a secure key provisioning mechanism (i.e., using Intel SGX Remote Attestation).
      */
     public static final String STREAM_KEY_HEX = "a46bf317953bf1a8f71439f74f30cd889ec0aa318f8b6431789fb10d1053d932";
+
+    /**
+     * Minimum log level for enclave logging (fixed value).
+     */
+    public static final LogLevel LOG_LEVEL = LogLevel.INFO;
+
+    public enum LogLevel {
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR
+    }
 }
