eba271fba148c7cb3b517387b8bb247d4109560f
Luca Di Bello <lucadibello@proton.me>
2025-12-27 13:33:21 +0000

    feat: created new utility command to build + reinstall TeaClave SDK from the patched fork

--- .devcontainer/teaclave-rebuild-install.sh
+++ .devcontainer/teaclave-rebuild-install.sh
@@ -2,25 +2,51 @@
 
 # This script automatically pulls + rebuilds and install a Teaclave SDK from the patched repo
 
-JAVA_HOME=$GRAAL_VM_HOME
 URL=https://github.com/lucadibello/teaclave-java-tee-sdk.git
 TEMP_DIR=$(mktemp -d)
 DEST_DIR=/opt/javaenclave
 
+# ensure graalvm is installed
+if [ -z "$GRAALVM_HOME" ]; then
+    echo "GRAALVM_HOME is not set. Please install GraalVM and set GRAALVM_HOME environment variable."
+    exit 1
+fi
+
+if [ ! -d "$GRAALVM_HOME" ]; then
+    echo "GRAALVM_HOME directory does not exist: $GRAALVM_HOME"
+    exit 1
+fi
+
+if [ ! -f "$GRAALVM_HOME/bin/java" ]; then
+    echo "GraalVM java binary not found in GRAALVM_HOME: $GRAALVM_HOME/bin/java"
+    exit 1
+fi
+
 set -e
 
 # clone patched sdk from github
-git clone --depth 1 --branch master $URL $TEMP_DIR
+git clone --branch master --depth 1 https://github.com/lucadibello/teaclave-java-tee-sdk.git /tmp/teaclave-java-tee-sdk
 
-cd $TEMP_DIR
+# Install GraalVM processor as Maven artifact
+/usr/local/bin/mvn install:install-file \
+    -DgroupId=org.graalvm.compiler \
+    -DartifactId=graal-processor \
+    -Dversion=22.2.0 \
+    -Dpackaging=jar \
+    -Dfile="${GRAALVM_HOME}/lib/graal/graal-processor.jar"
 
-# build and install teaclave sdk
-mkdir -p $DEST_DIR
-mvn -f ${TEMP_DIR}/sdk/pom.xml clean install -Pnative -DskipTests
-cp -r ${TEMP_DIR}/sdk/native/bin $DEST_DIR; \
-cp -r ${TEMP_DIR}/sdk/native/config $DEST_DIR; \
-cp -r ${TEMP_DIR}/sdk/native/script/build_app $DEST_DIR; \
-rm -rf $TEMP_DIR
-echo "Teaclave Java TEE SDK has been successfully installed to $DEST_DIR"
+# Pre-download all dependencies and plugins
+/usr/local/bin/mvn -f /tmp/teaclave-java-tee-sdk/sdk/pom.xml \
+    dependency:resolve \
+    dependency:resolve-plugins
+
+# Build and install teaclave sdk
+mkdir -p /opt/javaenclave
+mvn -f /tmp/teaclave-java-tee-sdk/sdk/pom.xml clean install -Pnative -DskipTests
+cp -r /tmp/teaclave-java-tee-sdk/sdk/native/bin /opt/javaenclave
+cp -r /tmp/teaclave-java-tee-sdk/sdk/native/config /opt/javaenclave
+cp -r /tmp/teaclave-java-tee-sdk/sdk/native/script/build_app /opt/javaenclave
+rm -rf /tmp/teaclave-java-tee-sdk
+echo "Teaclave Java TEE SDK has been successfully installed to /opt/javaenclave"
 
 exit 0
