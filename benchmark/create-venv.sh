afbc538a8753167a246729d61826695b3a9f77a2
Luca Di Bello <lucadibello@proton.me>
2026-03-11 19:03:11 +0100

    feat: updated jupyter notebook + environment

--- benchmark/create-venv.sh
+++ benchmark/create-venv.sh
@@ -1,4 +1,3 @@
-
 # create venv using venv module
 python3 -m venv .venv
 
@@ -8,4 +7,8 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 # create data directory if it doesn't exist
-mkdir -p data 
+mkdir -p data
+
+# initialize ipykernel from the venv
+python -m ipykernel install --user --name confidentialstorm --display-name "Python (confidentialstorm)"
+
